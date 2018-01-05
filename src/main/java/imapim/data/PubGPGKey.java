3f4dc218fc4f97ccb7db6972803288dd9aab4470
edwardfang <fangyd1997@outlook.com>
2018-01-05 22:04:07 +0800

    add search key on MIT GPG server

---
+++ src/main/java/imapim/data/PubGPGKey.java
@@ -0,0 +1,34 @@
+package imapim.data;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class PubGPGKey{
+    private final String fingerPrint;
+    private final String  userID;
+    private final String date;
+
+    public String getFingerPrint() {
+        return fingerPrint;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public PubGPGKey(String fingerPrint, String userID, String date ){
+        this.date = date;
+        this.fingerPrint =fingerPrint;
+        this.userID  = userID;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%s\n%s\n%s\n", userID, date, fingerPrint);
+    }
+}
