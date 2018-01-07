e00e2dec046b356e1b6e1140efaddd052cce3a53
edwardfang <fangyd1997@outlook.com>
2018-01-07 16:40:31 +0800

    add double click event in search table; finish search

--- src/main/java/imapim/ui/contact/ContactController.java
+++ src/main/java/imapim/ui/contact/ContactController.java
@@ -3,7 +3,7 @@ package imapim.ui.contact;
 import imapim.data.Person;
 import imapim.security.AESHelper;
 import imapim.ui.StageController;
-import imapim.ui.im.SettingController;
+import imapim.ui.util.SettingController;
 import imapim.ui.pgp.GeneratorController;
 import imapim.ui.util.PasswordDialog;
 import javafx.collections.FXCollections;
